76022829026a734466afb8a9927b5c1c159c7dfb
Pau Guillen <paugctrabajo@gmail.com>
2024-02-22 13:30:32 +0100

    Modified menu

--- proyect/src/main/java/com/controlescolar/Main.java
+++ proyect/src/main/java/com/controlescolar/Main.java
@@ -1,7 +1,36 @@
 package com.controlescolar;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        int option = 0;
+        System.out.println("Welcome to the MySQL CRUD menu!");
+        System.out.println("What would you like to do?");
+        while(!exit){
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    createCareer();
+                    break;
+                case 2:
+                    updateCareer();
+                    break;
+                case 3:
+                    readCareer();
+                    break;
+                case 4:
+                    deleteCareer();
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Please select an existing option.");
+                    break;
+            }
+        }
     }
 }
